26e44cfdffa57052502de7b2c7c985d30d923be3
James O'Brien <jaimzo@gmail.com>
2014-08-18 13:02:08 +0100

    Initial Commit

---
+++ echochamber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  echochamber
+//
+//  Created by James O'Brien on 18/08/2014.
+//  Copyright (c) 2014 James O'Brien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ echochamberTests/echochamberTests.m
@@ -0,0 +1,34 @@
+//
+//  echochamberTests.m
+//  echochamberTests
+//
+//  Created by James O'Brien on 18/08/2014.
+//  Copyright (c) 2014 James O'Brien. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface echochamberTests : XCTestCase
+
+@end
+
+@implementation echochamberTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
